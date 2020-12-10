ff6982dbd2082ad424f9724d2c28e4da0e792429
Gopikrishna Sathyamurthy <Gopikrishna19@users.noreply.github.com>
2020-12-09 21:58:20 -0600

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -5,3 +5,4 @@ apk upgrade
 apk del wget git
 apk add wget git nano openssh
 echo "export TERM=xterm" >> /etc/profile
+wget -qO --user=ssh-user --ask-password - http://ssh-server.rg-10.hm/ssh/install.sh | ash
