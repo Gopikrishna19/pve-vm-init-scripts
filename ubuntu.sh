dd2d6b9a247ab41dfab36bade22d404b172ebd7a
Gopikrishna Sathyamurthy <Gopikrishna19@users.noreply.github.com>
2020-12-14 22:46:50 -0600

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,6 @@
 
 apt update
 apt dist-upgrade
-apt remove wget git
-apt install wget git nano openssh
+apt remove -y wget git
+apt install -y wget git nano openssh
 wget --user=ssh-user --ask-password -qO - http://ssh-server.rg-10.hm/ssh/install.sh | bash
